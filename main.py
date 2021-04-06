d67405b8e979412a7b80befb36d554434e30061a
Deimer Estiven Estiven <>
2021-04-06 05:23:02 +0000

    bbbb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openpyxl
 import datetime
-import time
+import datetime as dt
 
 fechas=openpyxl.load_workbook('alumnos.xlsx')
 
@@ -10,8 +10,15 @@ inicio=fechas.active
 
 mi_fecha = datetime.datetime(2016, 10, 1, 23,50, 1, 1) 
 
-for i in range(1,4):
+inicio['C1']='Fecha personalizada'
+for i in range(2,5):
   inicio[f'C{i}']=mi_fecha
+
+fecha_ajustada = dt.datetime.utcnow() +dt.timedelta(hours=-5)
+
+inicio['D1']='Fecha Ajustada'
+for i in range(2,5):
+  inicio[f'D{i}']=fecha_ajustada
 
 fechas.save('Minuevoarchivo.xlsx')
 
