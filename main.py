efa8a6eaba96a5b6e4bba08f5ea5628dfe5272ee
Deimer Estiven Estiven <>
2021-04-06 05:15:02 +0000

    aa

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import openpyxl
-import datetime as dt
+import datetime
 import time
 
 fechas=openpyxl.load_workbook('alumnos.xlsx')
 
 inicio=fechas.active
 
-dia=time.strftime('%x')
+#dia=time.strftime('%x')
 
+mi_fecha = datetime.datetime(2016, 10, 1, 23,50, 1, 1) 
 
 for i in range(1,4):
-  inicio[f'C{i}']=dia
+  inicio[f'C{i}']=mi_fecha
 
 fechas.save('Minuevoarchivo.xlsx')
 
